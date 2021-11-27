277bf95d7b0bfd6d48f2da779ddb7e778047f68f
jrtrevino <j.trevino95@icloud.com>
2021-11-27 13:59:03 -0800

    added code to generate csvs from matchid csvs

--- src/preprocessing/parsing_wrapper.py
+++ src/preprocessing/parsing_wrapper.py
@@ -1,30 +1,40 @@
 import argparse
 import pandas as pd
 import parse_dota_api as dota
+import sys
 import time
 
 
-
 def run(match_id_list, api_key):
-    df_array = []
+    game_array = []
+    player_array = []
     error_array = []
     for match_id in match_id_list:
         # grab data from api
         responses = dota.get_match_by_id([match_id], api_key)
         if responses:
             print("Grabbed match data from game: {}".format(match_id))
-            df_array.append(pd.json_normalize(responses))
+            # our response dataframe 'master'
+            df = pd.json_normalize(responses[0])
+            df.drop('players', inplace=True, axis=1)
+            df.drop('picks_bans', inplace=True, axis=1)
+            # normalize players column
+            players = pd.json_normalize(responses[0], record_path=["players"], record_prefix="players.")
+            game_array.append(df)
+            player_array.append(players)
         else:
             print("Could not parse data from game: {}".format(match_id))
             error_array.append(match_id)
         time.sleep(1) # for api throttle limit
-    df_to_csv = pd.concat(df_array)
-    return df_to_csv, error_array
+    game_to_csv = pd.concat(game_array)
+    players_to_csv = pd.concat(player_array)
+    return game_to_csv, players_to_csv, error_array
 
 
-def df_to_csv(output_name, df, error_array):
+def df_to_csv(output_name, df_game, df_players, error_array):
     try:
-        df.to_csv(output_name, mode="w+")
+        df_game.to_csv("game-{}".format(output_name), mode="w+")
+        df_players.to_csv("players-{}".format(output_name), mode="w+")
         if len(error_array) > 0:
             with open(output_name[:-3] + "-error.csv", "w+") as f:
                 for match_id in error_array:
@@ -32,16 +42,28 @@ def df_to_csv(output_name, df, error_array):
     except Exception as e:
         print(e)
         print("Error making csv. Attempting to write to ./temp")
-        df.to_csv('./temp.csv', 'w+')
-    
+        df_game.to_csv('./temp-game-{}'.format(output_name), 'w+')
+        df_players.to_csv('./temp-players-{}'.format(output_name), 'w+')
+
+
+def load_file(filename):
+    try:
+        with open(filename, 'r') as f:
+            match_ids = f.readlines() 
+    except:
+        print("Could not open file. Try again.")
+        sys.exit(-1)
+    return [stripped.rstrip() for stripped in match_ids[1:]]
 
 
 if __name__ == "__main__":
-    api_key = dota.load_api_key('API_KEY')
-    df, errors = run([6288572418,
-         6288572419,
-         6289096708,
-         6289096709,
-         6289096711,
-         6289096714], api_key)
-    df_to_csv('testrun.csv',df, errors)
+    parser = argparse.ArgumentParser(description='Generate CSV files from OpenDota match ids.')
+    parser.add_argument('input', type=str, help="Input file path.")
+    parser.add_argument('output', type=str, help="Output file path.")
+    parser.add_argument('key', type=str, help="Your dota API key")
+    args = parser.parse_args()
+    if not args.key:
+        api_key = dota.load_api_key('API_KEY')
+    match_ids = load_file(args.input)
+    df_game, df_players, errors = run(match_ids[:3], args.key if args.key else api_key)
+    df_to_csv(args.output ,df_game, df_players, errors)
